c2f5c85b27f9e307eb07fd9014330edd3dfa6002
yuvraj <yuvrajsab@protonmail.com>
2023-05-03 19:24:22 +0530

    remove stuff that was ignored

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/yuvraj/code/samarth-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/yuvraj/code/samarth-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/yuvraj/code/samarth-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/yuvraj/code/samarth-docs/src/css/custom.css'),
-];
--- build/assets/js/1e9ea5f0.54855dd2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmydocs=self.webpackChunkmydocs||[]).push([[649],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
